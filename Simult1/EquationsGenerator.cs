a5a8eaa8e57ab9d187f05bc27443c095db5750f4
Adrian Sims <adriansims62781@gmail.com>
2019-02-13 20:31:09 +0100

    Add project files.

---
+++ Simult1/EquationsGenerator.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Simult1
+{
+    //   | a  b |      | x |     | m |
+    //   |      |   *  |   |  =  |   |
+    //   | c  d |      | y |     | n |
+    //
+    //   determinant = ad - bc.
+    //
+    //   inverse =    1    | d  -b |
+    //               ---   |       |
+    //                d    | -c  a |
+    //
+
+    class EquationsGenerator
+    {
+        private readonly Random rnd = new Random();
+
+        private int NonZeroRandom()
+        {
+            var sign = (rnd.Next(0, 2) * 2) - 1;
+            return rnd.Next(1, 20) * sign;
+        }
+
+        public IEnumerable<(int a, int b, int c, int d, int m, int n, double x, double y)> Generator()
+        {
+            while (true)
+            {
+                var a = NonZeroRandom();
+                var b = NonZeroRandom();
+                var c = NonZeroRandom();
+                var d = NonZeroRandom();
+                var e = NonZeroRandom();
+                var m = rnd.Next(-20, 20);
+                var n = rnd.Next(-20, 20);
+
+                var det = a * d - b * c;
+                if (det == 0)
+                {
+                    continue;
+                }
+
+                var mulx = d * m - b * n;
+                var muly = -c * m + a * n;
+
+                if (det == 2 || (mulx % det == 0 && mulx % det == 0))
+                {
+                    yield return (a, b, c, d, m, n, mulx / det, muly / det);
+                }
+            }
+        }
+
+    }
+}
---
+++ Simult1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simult1
+{
+    class Program
+    {
+
+        //private static string GetVar(int x)
+        //{
+        //    if (x == 1)
+        //    {
+        //        return "x";
+        //    }
+        //    if (x == -1)
+        //    {
+        //        return "-x";
+        //    }
+        //    return $"{x}x";
+        //}
+
+        private static void PrintEqn(int a, int b, int c, int d, int m, int n, double x, double y)
+        {
+            Console.WriteLine($"{a} {b} {c} {d} {m} {n}  - {x} {y}");
+        }
+
+        private static void Main(string[] args)
+        {
+            try
+            {
+                var generator = new EquationsGenerator();
+                foreach (var (a, b, c, d, m, n, x, y) in generator.Generator().Take(100))
+                {
+                    PrintEqn(a, b, c, d, m, n, x, y);
+                }
+            }
+            catch (Exception ex)
+            {
+                var fullname = System.Reflection.Assembly.GetEntryAssembly().Location;
+                var progname = Path.GetFileNameWithoutExtension(fullname);
+                Console.Error.WriteLine(progname + ": Error: " + ex.Message);
+            }
+
+        }
+    }
+}
